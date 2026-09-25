f8b13f8d84ff814ce7aa4f1d1d00216d7612da13
mongisididua-creator <mongisididua@gmail.com>
2026-09-25 18:22:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CF_DNS_CONFIG = {
   // Tempel URL deployment Google Apps Script yang berakhir dengan /exec
-  APPS_SCRIPT_URL: ''
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbziGKnxLrWZQuynIa_RKqVdA3mnAUizQ83msrZJJbnY-M4PoK4MTNyHOlsZU-iL6TdB/exec'
 };
